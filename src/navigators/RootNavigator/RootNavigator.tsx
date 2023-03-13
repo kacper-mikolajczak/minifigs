a3ff6e2f6becb4724ba5f7adbe9e69a063d07943
KacperM717 <kacpermikolajczak10@gmail.com>
2023-03-13 20:55:38 +0100

    add example test case

--- src/navigators/RootNavigator/RootNavigator.tsx
+++ src/navigators/RootNavigator/RootNavigator.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {HomeScreen} from 'src/screens/Home/Home.screen';
 
 const Stack = createNativeStackNavigator();
 
 export const RootNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Home" component={() => null} />
+      <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
   );
 };
---
+++ src/screens/Home/Home.screen.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import {Text} from 'react-native';
+
+export const HomeScreen = () => {
+  return <Text testID="test-text">This is a test home component</Text>;
+};
