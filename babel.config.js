906112a1d7a79bf4cf07bc3e3896438a7bcafb5b
Kacper Mikołajczak <62747088+kacper-mikolajczak@users.noreply.github.com>
2023-03-13 11:25:49 +0100

    feat(setup): env variables
    * install react-native-config
    
    * add env variables
    
    * android fix
    
    * add .env to gitignore
    
    * add env config

--- babel.config.js
+++ babel.config.js
@@ -8,6 +8,7 @@ module.exports = {
         extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
         alias: {
           '@assets': ['./assets'],
+          '@config': ['./src/config'],
           '@components': ['./src/components'],
         },
       },
