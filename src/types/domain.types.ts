6a5631f92b244fe9b9356bf1f2ba1abbb317d2de
KacperM717 <kacpermikolajczak10@gmail.com>
2023-03-13 11:52:23 +0100

    add domain types

---
+++ src/types/domain.types.ts
@@ -0,0 +1,27 @@
+declare global {
+  namespace Domain {
+    type Minifig = {
+      setId: string;
+      name: string;
+      img: string | null;
+    };
+
+    type Address = {
+      street: string;
+      city: string;
+      state: string;
+      zipCode: string;
+    };
+
+    type User = {
+      firstName: string;
+      lastName: string;
+      email: string;
+      phoneNumber: string;
+      dateOfBirth: Date;
+    };
+
+    type UserShippingInfo = User & Address;
+  }
+}
+export {};
