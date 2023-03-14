963baa91302e3a8f3a262d3c7546f00f79be5c3d
KacperM717 <kacpermikolajczak10@gmail.com>
2023-03-14 23:30:25 +0100

    add queryClient to GlobalProvider

--- src/providers/GlobalProvider.tsx
+++ src/providers/GlobalProvider.tsx
@@ -1,6 +1,13 @@
-import {NavigationContainer} from '@react-navigation/native';
 import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export const GlobalProvider = ({children}: React.PropsWithChildren) => {
-  return <NavigationContainer>{children}</NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </NavigationContainer>
+  );
 };
