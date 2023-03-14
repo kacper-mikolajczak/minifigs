f45bad4568ea14bd9124f6fc639f26bdf895ae35
KacperM717 <kacpermikolajczak10@gmail.com>
2023-03-14 23:20:20 +0100

    add randoMinifig query

---
+++ src/loaders/queries/minifigs/useRandomMinifigQuery.ts
@@ -0,0 +1,22 @@
+import {
+  QueryFunctionContext,
+  useQuery,
+  UseQueryOptions,
+} from '@tanstack/react-query';
+import {RebrickableAPI} from '@api/rebrickable/rebrickable.api';
+import {minifigsQueryKeys, MinifigsQueryKeys} from './queryKeys';
+
+type QueryKey = [MinifigsQueryKeys['search']];
+type QueryFn = typeof queryFn;
+type QueryFnRT = Awaited<ReturnType<QueryFn>>;
+const queryFn = async ({queryKey}: QueryFunctionContext<QueryKey>) => {
+  const [{search}] = queryKey;
+  return RebrickableAPI.searchRandomMinifig(search);
+};
+
+export const useRandomMinifigQuery = <D = QueryFnRT>(
+  search: string,
+  options?: UseQueryOptions<QueryFnRT, unknown, D, QueryKey>,
+) => {
+  return useQuery([minifigsQueryKeys.search(search)], queryFn, options);
+};
--- src/screens/minifigOdrer/MinifigOrder.tsx
+++ src/screens/minifigOdrer/MinifigOrder.tsx
@@ -1,9 +1,11 @@
+import React from 'react';
 import {ScrollView, StyleSheet} from 'react-native';
 import {Heading} from '@components/typography/heading/Heading';
 import {useTranslation} from 'react-i18next';
 import {RandomMinifigPreview} from './partials/randomMinifigPreview/RandomMinifigPreview';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '@navigators/RootNavigator/RootNavigator';
+import {useRandomMinifigQuery} from 'src/loaders/queries/minifigs/useRandomMinifigQuery';
 
 type MinifigOrderScreenProps = NativeStackScreenProps<
   RootStackParamList,
@@ -13,10 +15,15 @@ type MinifigOrderScreenProps = NativeStackScreenProps<
 export const MinifigOrderScreen = ({route}: MinifigOrderScreenProps) => {
   const {search} = route.params;
 
+  const randomMinifigQuery = useRandomMinifigQuery(search);
+
   return (
     <ScrollView style={styles.container}>
       <ScreenTitle />
-      <RandomMinifigPreview />
+      <RandomMinifigPreview
+        minifig={randomMinifigQuery.data}
+        onDraw={randomMinifigQuery.refetch}
+      />
     </ScrollView>
   );
 };
