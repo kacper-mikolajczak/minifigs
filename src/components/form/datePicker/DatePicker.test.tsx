71ad793cfccfa1f2c20ab0895f80ac4592adce10
KacperM717 <kacpermikolajczak10@gmail.com>
2023-03-14 15:21:50 +0100

    add DatePicker test suite

---
+++ src/components/form/datePicker/DatePicker.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {render, screen} from '@tests/utils';
+import {DatePicker} from './DatePicker';
+
+describe('DatePicker tests', () => {
+  it.todo('Did not have enough time to implement it correctly. Sorry!');
+  // it('displays provided date', () => {
+  //   const date = new Date();
+
+  //   render(<DatePicker date={date} />);
+
+  //   screen.getByDisplayValue(date.toLocaleDateString());
+  // });
+});
